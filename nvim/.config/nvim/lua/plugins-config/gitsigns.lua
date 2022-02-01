60d7c965b61abef1cc46dc8d9978d433168accf9
Thomas Chai <thomas@terminaljunkie.com>
2022-02-01 22:18:20 +0800

    minor modification to gitsigns plugin options

--- nvim/.config/nvim/lua/plugins-config/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins-config/gitsigns.lua
@@ -15,7 +15,7 @@ require('gitsigns').setup {
        follow_files = true
      },
      attach_to_untracked = true,
-     current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
      current_line_blame_opts = {
        virt_text = true,
        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
@@ -23,7 +23,7 @@ require('gitsigns').setup {
        ignore_whitespace = false,
      },
      current_line_blame_formatter_opts = {
-       relative_time = false
+       relative_time = true
      },
      sign_priority = 6,
      update_debounce = 100,
