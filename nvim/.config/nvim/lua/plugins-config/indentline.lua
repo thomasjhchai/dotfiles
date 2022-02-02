9824ba2d8c27772ab9b57a17c714e37ebc55b347
Thomas Chai <thomas@terminaljunkie.com>
2022-02-03 00:57:28 +0800

    minor modification

--- nvim/.config/nvim/lua/plugins-config/indentline.lua
+++ nvim/.config/nvim/lua/plugins-config/indentline.lua
@@ -1,3 +1,7 @@
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
     show_current_context = true,
