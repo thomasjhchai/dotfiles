48e3354f3d67e819ad53140faa28665ecc2063be
Thomas Chai <thomas@terminaljunkie.com>
2024-05-10 10:51:57 +0800

    added mac deployment README and improve the deployment script

---
+++ bin/mac_deploy.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Set the directory where your dotfiles are located
+DOTFILES_DIR="$HOME/dotfiles"
+
+# Make a dotfiles directory if it doesn't already exist
+if [! -d "$DOTFILES_DIR" ]; then
+    mkdir -p "$DOTFILES_DIR"
+    echo "Created $DOTFILES_DIR"
+else
+    echo "$DOTFILES_DIR already exists"
+fi
+
+# Check if Xcode Command Line Tools are installed
+if ! xcode-select --install &> /dev/null; then
+    echo "Xcode Command Line Tools are not installed. Installing now..."
+
+    # Install Xcode Command Line Tools
+    sudo xcode-select --install
+
+    # Wait for the installation to complete
+    while sudo /usr/bin/xcode-select --install &> /dev/null; do
+        echo "Installing Xcode Command Line Tools..."
+        sleep 5
+    done
+
+    echo "Xcode Command Line Tools installation complete."
+else
+    echo "Xcode Command Line Tools are already installed."
+fi
+
+
+# Install Homebrew if not already installed
+echo "Installing Homebrew"
+    if ! [ -x "$(command -v brew)" ]; then
+    echo "Installing Homebrew"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+# Install apps from Brewfile
+if [ -f "$HOME/Brewfile" ]; then
+    echo "Installing apps from Brewfile..."
+    brew bundle --file="$HOME/Brewfile"
+else
+    echo "Brewfile not found in $HOME. Skipping app installation."
+fi
+ 
+
+# Set the list of packages (directories) to be managed by Stow
+PACKAGES=(alacritty karabiner fd git nvim tmux zsh)
+
+# Function to link a package using Stow
+link_package() {
+    local package="$1"
+    
+    # Check if the package directory exists
+    if [ ! -d "$DOTFILES_DIR/$package" ]; then
+        echo "Error: Package directory '$package' not found in '$DOTFILES_DIR'"
+        return 1
+    fi
+    
+    # Link the package
+    stow -vt "$HOME" "$package" -d "$DOTFILES_DIR"
+    echo "Linked package '$package'"
+}
+
+# Link all packages
+echo "Linking all packages..."
+for package in "${PACKAGES[@]}"; do
+    link_package "$package"
+done
+
+echo "All packages have been linked."
+echo "All installation steps have been completed."
+
+# TODO: double check below
+
+# install ohmyzsh if .ohmyzsh folder doesn't exist
+if [! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "oh-my-zsh installed"
+else
+    echo "oh-my-zsh already installed"
+fi
+
+# install zsh-autosuggestions
+if [! -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "zsh-autosuggestions installed"
+    echo "zsh-autosuggestions enabled"
+fi
+
+# install zsh-syntax-highlighting
+if [! -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-
+    echo "zsh-syntax-highlighting installed"
+    echo "zsh-syntax-highlighting enabled"
+fi
+
+# install autojump
+if [! -d "$HOME/.oh-my-zsh/plugins/autojump" ]; then
+    git clone https://github.com/wting/autojump.git "$HOME/.oh-my-zsh/plugins/autojump"
+    echo "autojump installed"
+    echo "autojump enabled"
+fi
+
+# install ohmyzsh spaceship theme
+if [! -d "$HOME/.oh-my-zsh/themes/spaceship-prompt" ]; then
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt"
+    echo "spaceship-prompt installed"
+    echo "spaceship-prompt enabled"
+fi
+
+
+
+
+
+
+
+
